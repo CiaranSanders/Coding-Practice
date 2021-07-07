e72effd0779b2a152e336a754a1d814bf544c28e
CiaranSanders <ciaransanders@gmail.com>
2021-07-06 17:23:09 -0700

    added leetcode problemn 1338

---
+++ leetcode/1338/reduce_array_size_to_half.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    result = sol.minSetSize()
+    result = sol.minSetSize([1,2,3,3])
+    print(result)
+
