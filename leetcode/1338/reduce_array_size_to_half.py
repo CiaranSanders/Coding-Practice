e330ae909bc37536fe97c4e9c7f35cf901c7c7ca
CiaranSanders <ciaransanders@gmail.com>
2021-07-06 17:36:22 -0700

    working solution, but its over complicated since it turns out in the problem description arr is always guaranteed to be even!

--- leetcode/1338/reduce_array_size_to_half.py
+++ leetcode/1338/reduce_array_size_to_half.py
@@ -1,11 +1,35 @@
+from typing import List
+from collections import Counter
+from math import ceil
+
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        pass
+        count = Counter(arr)
+        half = ceil(len(arr) / 2)
+        num = 0
+        total = 0
+        for val, count in count.most_common():
+            total += count
+            num += 1
+            if total >= half:
+                return num
+        return 0 
 
 
 if __name__ == '__main__':
     sol = Solution()
-    result = sol.minSetSize()
-    result = sol.minSetSize([1,2,3,3])
-    print(result)
+    tests = [
+        [7,7,7,7,7,7],  # answer: 1
+        [1,2,3,4,5],  # answer: 3
+        [1,2,3,4],  # answer: 2
+        [1000,1000,1,1],  # answer: 1
+        [1000,1000,1,2],  # answer: 1
+        [],  # answer: None ? or 0?
+        [1],  # answer: 1
+        [3,4,5,6,7,6,4,2,1,2,2,2,3,45],  # answer: 3
+        [1234,1234],  # answer: 1
+    ]
+    for test in tests:
+        result = sol.minSetSize(test)
+        print(result)
 
